aa011b3118c5c38ec38fabb504de1aad2ab74d87
alex <alex@mail.com>
2023-11-04 14:57:59 +0300

    templates for

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,18 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'username': 'Алексей'}
-    return render_template('index.html', title='Главная', user=user)
+    posts = [
+        {
+            'author': {'username': 'Вадим'},
+            'body': 'Сегодня я распечатал свою коробочку!'
+        },
+        {
+            'author': {'username': 'Миша'},
+            'body': 'Я сделал игру на Scratch!'
+        }, 
+        {
+            'author': {'username': 'Иван'},
+            'body': 'А я делал конструкции на базе Arduino!'
+        }
+    ]
+    return render_template('index.html', title='Главная', user=user, posts=posts)
