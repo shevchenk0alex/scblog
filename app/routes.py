6c50e88db208aebd16fb74f197972c882ede810b
alex <alex@mail.com>
2023-11-04 14:30:28 +0300

    templates_1

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Aлексей'}
+    return '''
+<html>
+    <head>
+        <title>Главная страница - schoolblog</title>
+    </head>
+    <body>
+        <h1>Привет, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
