db362bc53907a7d2120c2a88a8f332af033a5342
alex <alex@mail.com>
2023-11-04 15:57:30 +0300

    forms url for

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 @app.route('/')
@@ -26,7 +26,11 @@ def index():
 def about():
     return "Еще не придумали"
     
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Вход', form=form)
