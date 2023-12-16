5c7e1fabb82e8db0aeb2aa4ee0a8cf9e1dd7e102
alex <alex@mail.com>
2023-12-16 09:56:15 +0300

    db_init

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+#print(basedir)
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '12345678'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')   
