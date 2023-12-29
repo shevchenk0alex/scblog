19a768077c0dde4b221d3b77a2a0f5b07b0d7014
alex <alex@mail.com>
2023-12-29 15:39:30 +0300

    add registr

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+import sqlalchemy as sa
+from app import db
+from app.models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    email = StringField('Электронная почта', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Повторите пароль', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Регистрация')
+
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(
+            User.username == username.data))
+        if user is not None:
+            raise ValidationError('Данный логин занят. Введите уникальный логин')
+
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(
+            User.email == email.data))
+        if user is not None:
+            raise ValidationError('Данная почта уже используется. Введите другой адрес')
 
 class LoginForm(FlaskForm):
     username = StringField('Логин', validators=[DataRequired()])
