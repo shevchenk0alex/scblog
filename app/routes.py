8c33d40c9ce34e9132335ea92869db6e7cff0926
alex <alex@mail.com>
2024-01-04 12:51:43 +0300

    add route user

--- app/routes.py
+++ app/routes.py
@@ -82,3 +82,13 @@ def register():
         flash('Поздравляю, теперь вы зарегистрированный пользователь!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+    
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #1'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
