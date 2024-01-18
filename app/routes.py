f9de70784039cf506865f38447dab637c873df02
alex <alex@mail.com>
2024-01-18 13:50:51 +0300

    add Pagination

--- app/routes.py
+++ app/routes.py
@@ -29,8 +29,14 @@ def index():
         flash('Ваше сообщение опубликовано!')
         return redirect(url_for('index'))
     
-    posts = db.session.scalars(current_user.following_posts()).all()
-    return render_template('index.html', title='Главная', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = db.paginate(current_user.following_posts(), page=page,
+                        per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Главная', form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
     
 @app.route('/about')
 def about():
@@ -82,12 +88,18 @@ def register():
 @login_required
 def user(username):
     user = db.first_or_404(sa.select(User).where(User.username == username))
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #1'}
-    ]
+    page = request.args.get('page', 1, type=int)
+    query = user.posts.select().order_by(Post.timestamp.desc())
+    posts = db.paginate(query, page=page,
+                        per_page=app.config['POSTS_PER_PAGE'],
+                        error_out=False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
+    return render_template('user.html', user=user, posts=posts.items, 
+        next_url=next_url, prev_url=prev_url,form=form)
     
 @app.route('/edit_profile', methods=['GET', 'POST'])    
 @login_required
@@ -149,6 +161,12 @@ def unfollow(username):
 @app.route('/flow')
 @login_required
 def flow():
+    page = request.args.get('page', 1, type=int)
     query = sa.select(Post).order_by(Post.timestamp.desc())
-    posts = db.session.scalars(query).all()
-    return render_template('index.html', title='Поток', posts=posts)
+    posts = db.paginate(query, page=page,
+                        per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('flow', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('flow', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Поток', posts=posts.items, next_url=next_url, prev_url=prev_url)
--- config.py
+++ config.py
@@ -4,6 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '12345678'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    
+    POSTS_PER_PAGE = 25
 
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
