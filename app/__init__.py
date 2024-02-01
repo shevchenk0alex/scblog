fc8e7cdfedab8ff483d4effe960285f88661d9b7
alex <alex@mail.com>
2024-02-01 16:15:43 +0300

    fix login flash

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = "Пожалуйста, войдите, чтобы открыть эту страницу."
 mail = Mail(app)
 
 if not app.debug:
