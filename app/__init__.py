f32c876f0bf985ba268000e725aa6b689209fd45
alex <alex@mail.com>
2024-02-03 11:05:23 +0300

    add get_locale

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
+from flask_babel import Babel
 
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
 import os
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,6 +24,7 @@ login.login_view = 'login'
 login.login_message = "Пожалуйста, войдите, чтобы открыть эту страницу."
 mail = Mail(app)
 moment = Moment(app)
+babel = Babel(app, locale_selector=get_locale)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    LANGUAGES = ['ru', 'en']
+
     SECRET_KEY = os.environ.get('SECRET_KEY') or '12345678'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     
