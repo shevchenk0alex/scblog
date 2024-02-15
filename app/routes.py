750a56d5d5295c992fabb5c83e6e1cfd4e4f05ec
alex <alex@mail.com>
2024-02-15 17:17:52 +0300

    add lang in model

--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,7 @@ from flask import request
 from urllib.parse import urlsplit
 from datetime import datetime, timezone
 from app.email import send_password_reset_email
+from langdetect import detect, LangDetectException
 
 @app.before_request
 def before_request():
@@ -24,7 +25,11 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        try:
+            language = detect(form.post.data)
+        except LangDetectException:
+            language = ''
+        post = Post(body=form.post.data, author=current_user, language=language)
         db.session.add(post)
         db.session.commit()
         flash('Ваше сообщение опубликовано!')
